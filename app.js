9ca68a1ebbadd8dd797febb1712d6a417aab16fe
MAIRA ALDANA Da silva <ALDANANA.DS@GMAIL.COM>
2025-08-05 19:23:47 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+
+function agregarAmigo(){
+    if(inputAmigo.value){
+        
+    }else {
+            alert("Debes ingresar un nombre")
+        }
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+};
+
+function sortearAmigo(){
+    const random = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<l1> El amigo secreto es : ${amigoSecreto}</li>` 
+};
